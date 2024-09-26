23eedf4b900b2be0aabd8eb7d87ced3a8e4d3454
Dmitrii Liashko <mitya.lyashko.02@mail.ru>
2024-09-26 18:58:50 +0300

    Счётчик

---
+++ vue-base/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import MyHelloOptions from './components/MyHelloOptions.vue'
+import MyHelloComposition from './components/MyHelloComposition.vue'
+</script>
+
+<template>
+  <MyHelloOptions />
+  <MyHelloComposition />
+</template>
