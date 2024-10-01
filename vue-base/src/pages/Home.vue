a8536bf126f227a76c0e890fad619d86429ebb85
Dmitrii Liashko <mitya.lyashko.02@mail.ru>
2024-10-01 10:49:07 +0300

    Добавил фон страницы, зафиксировал header

--- vue-base/src/pages/Home.vue
+++ vue-base/src/pages/Home.vue
@@ -3,7 +3,17 @@ import Header from '../components/Header.vue'
 </script>
 
 <template>
-  <Header />
+  <Header class="fixed top-0 left-0 right-0" />
   <!-- <Body /> -->
+  <div class="bg-[url(/bull/the-bull.png)] bg-cover bg-no-repeat bg-fixed mt-20">
+    <img src="/bull/salat-mix.png" />
+    <img src="/bull/steak-ny.png" />
+    <img src="/bull/salat-mix.png" />
+    <img src="/bull/steak-ny.png" />
+    <img src="/bull/salat-mix.png" />
+    <img src="/bull/steak-ny.png" />
+    <img src="/bull/salat-mix.png" />
+    <img src="/bull/steak-ny.png" />
+  </div>
   <!-- <Bottom /> -->
 </template>
