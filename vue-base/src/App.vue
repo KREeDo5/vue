90420c83fe2c70338764205869ac8a973c0df510
Dmitrii Liashko <mitya.lyashko.02@mail.ru>
2024-10-03 16:16:26 +0300

    Вынес header в отдельный тег, поправил отступ

--- vue-base/src/App.vue
+++ vue-base/src/App.vue
@@ -3,9 +3,10 @@ import Header from './components/Header.vue'
 </script>
 
 <template>
-  <Header class="header fixed top-0 left-0 right-0 z-10" />
+  <header>
+    <Header class="header fixed top-0 left-0 right-0 z-10" />
+  </header>
   <main>
-    <div class="flex justify-center text-white bg-red-500">03.09-03.10 Скидка 20% на все блюда</div>
     <RouterView />
   </main>
 </template>
